6fe4b2cd9e92f807ea12c92c712e82ccd994e28f
oshan <oshanamarathunga1@gmail.com>
2024-02-26 11:11:44 +0530

    Version f1.0

--- Lovers_Leap/src/main/java/edu/ijse/lovers_leap/Main.java
+++ Lovers_Leap/src/main/java/edu/ijse/lovers_leap/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 666, 477);
         stage.setTitle("Log into Lovers Leap");
         stage.setScene(scene);
